84614968f3ccc91be5bf60d712c9e8fd174c8322
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-11-07 21:12:09 +0700

    FEAT & FIX : menambahkan fitur dan fix source code

--- config/app.php
+++ config/app.php
@@ -67,7 +67,10 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    // 'timezone' => 'UTC',
+    'timezone' => 'Asia/Jakarta',
+    'locale' => 'id',
+    'faker_locale' => 'id_ID',
 
     /*
     |--------------------------------------------------------------------------
