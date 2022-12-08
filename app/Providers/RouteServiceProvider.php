9af07131063d8811f59887650448e37001ee0ab4
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-12-08 09:19:42 +0700

    FIX : fix register member and redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = 'admin/dashboard';
+    public const MEMBER_HOME = 'dashboard';
 
     /**
      * The controller namespace for the application.
